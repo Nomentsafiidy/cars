f38b39395218eb16a75012d8193847a5ea02a3b6
Nomentsafidy <mnomentsafidy@gmail.com>
2021-07-07 22:34:17 +0300

    fix cars empty list

--- src/routes/car.ts
+++ src/routes/car.ts
@@ -1,17 +1,17 @@
 // user.ts
 import * as express from 'express';
 import { auth } from './../middleware/authorization';
-import { ApiCarsResponse, ApiResponse } from './../models/ApiResponse';
+import { ApiCarsResponse } from './../models/ApiResponse';
+import { carService } from './../services/car';
 const router = express.Router();
 
 router.get('/getCars', auth(), async (req: any, res) => {
     let response: ApiCarsResponse = { success: false, cars: [] };
     try {
         if (req.user) {
-            console.log('user', req.user);
-            console.log('**if**');
+            response = await carService.getCars(true);
         } else {
-            console.log('**else**');
+            response = await carService.getCars(false);
         }
     } catch (error) {
         response.message = '' + error;
